92e6205eff7bf76d5e5033cc82537e0bbd890253
Miguel Manalo <miguelmanalo@protonmail.com>
2020-09-01 17:19:53 -0400

    playing around

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+var unique = require('uniq');
+var Caman = require('caman').Caman;
+
+
 var myDate = new Date();
 console.log(myDate);
+
+Caman("pineapple.jpg", function () {
+  this.brightness(5);
+  this.render(function () {
+    this.save("pineapple2.png");
+  });
+});
